08ccfa52e75774f7ec071cab2fdf0d91fae0afe3
Veronica Lima <veris.ramires@gmail.com>
2020-08-06 08:43:44 -0300

    Estructure of project

---
+++ src/main/java/br/com/cbm/CloudBlueManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.cbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBlueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBlueManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cbm/CloudBlueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBlueManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
